8949f87bf426d8130252ac1b5d8b43f70ee0ebe2
Ak550214 <ironextreme5@gmail.com>
2024-06-30 22:11:48 +0530

    Update project4.sol

--- project4.sol
+++ project4.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.0.0/contracts/token/ERC20/ERC20.sol";
